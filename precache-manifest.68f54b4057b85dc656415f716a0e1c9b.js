1d1829879ae8932bd0cf21f1a922e287c51b90bf
Gogabok <gogabok123@gmail.com>
2023-11-26 17:40:22 +0300

    Update static

---
+++ precache-manifest.68f54b4057b85dc656415f716a0e1c9b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0bbe110f3cee156613fa16f6bb1ebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e89dd255ba7edb680121",
+    "url": "/static/css/main.43ca7237.chunk.css"
+  },
+  {
+    "revision": "4aa1ec6d8ed8971cdaa0",
+    "url": "/static/js/2.083eb24d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.083eb24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89dd255ba7edb680121",
+    "url": "/static/js/main.330368f8.chunk.js"
+  },
+  {
+    "revision": "702ea6b7dadb798376e5",
+    "url": "/static/js/runtime-main.5323ae90.js"
+  },
+  {
+    "revision": "0c6a404fd1628889d2b0f8206e2f3b37",
+    "url": "/static/media/SuisseIntl-Book-WebM.0c6a404f.eot"
+  },
+  {
+    "revision": "5f777e79ca58a3d3ae6f61d6dbea10af",
+    "url": "/static/media/SuisseIntl-Book-WebM.5f777e79.ttf"
+  },
+  {
+    "revision": "c20b7d1194bedb79d9c3d4e2d55941e3",
+    "url": "/static/media/SuisseIntl-Book-WebM.c20b7d11.woff"
+  },
+  {
+    "revision": "c749c5fbbb83e590263b8832a3ec1ef1",
+    "url": "/static/media/SuisseIntl-Book-WebM.c749c5fb.svg"
+  }
+]);
